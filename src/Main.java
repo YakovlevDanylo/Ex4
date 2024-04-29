b1892dc8684029deae6d5a11e837304725944862
YakovlevDanylo <yakovlevdv31@gmail.com>
2024-04-29 20:08:20 +0300

    Create method findSymbolOccurance

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(findSymbolOccurance("Hello world!", 'o'));
+    }
+
+    static public int findSymbolOccurance(String source, char symbol) {
+        int counter = 0;
+        for (int i = 0; i < source.length(); i ++) {
+            if(source.charAt(i) == symbol) {
+                counter++;
+            }
+        }
+            return counter;
     }
 }
